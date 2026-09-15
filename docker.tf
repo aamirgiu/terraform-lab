9ce3365aa046074d0764a453a7249896a179f031
aamir <aamir@localhost.localdomain>
2026-09-15 12:02:55 +0500

    Add terraform docker container

---
+++ docker.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///run/user/1000/podman/podman.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:alpine"
+}
+
+resource "docker_container" "nginx" {
+  name  = "terraform-nginx"
+  image = docker_image.nginx.image_id
+  
+  ports {
+    internal = 80
+    external = 8090
+  }
+}
