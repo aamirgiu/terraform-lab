b524475cf2a3f648993e25e03d8392a3f266966a
aamir <aamir@localhost.localdomain>
2026-09-17 15:21:27 +0500

    Add terraform variables

--- docker.tf
+++ docker.tf
@@ -16,11 +16,11 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  name  = "terraform-nginx"
+  name  = var.container_name
   image = docker_image.nginx.image_id
   
   ports {
     internal = 80
-    external = 8090
+    external = var.external_port
   }
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "container_name" {
+  description = "Container ka naam"
+  default     = "my-nginx"
+}
+
+variable "external_port" {
+  description = "Bahar ka port"
+  default     = 8091
+}
